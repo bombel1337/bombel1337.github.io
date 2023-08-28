e2b433440bf8afab0e154c2f8cd9abd5ff3f5543
bombel1337 <przygi997@gmail.com>
2023-08-28 13:35:59 +0200

    test

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from 'react'
 import './App.css';
 import { Header, About, Services, Experience, Projects, Contact } from './containers'
 import { Cursor } from 'custom-pointer-react' //https://harshhhdev.github.io/custom-pointer-react/
+import { BrowserView, MobileView, isBrowser, isMobile } from 'react-device-detect';
 
 
 
@@ -19,13 +20,15 @@ const App = () => {
           <Experience/>
           <Projects/>
           <Contact/>
+          <BrowserView>
           <Cursor
   showRing={true}
   color="#212121"
   ringSize={47}
   cursorSize={8}
   ringBorder={1}
-/>
+/></BrowserView>
+
     </div>
   )
 }
